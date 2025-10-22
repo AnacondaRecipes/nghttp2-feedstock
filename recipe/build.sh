c6c4725e357ee54750053111d4742395110d754b
Andrii Osipov <aosipov@anaconda.com>
2025-10-22 01:48:45 -0700

    PKG-10045: nghttp2 1.67.1

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,8 @@
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
 
-./configure --prefix=${PREFIX} --enable-python-bindings=no
+# Build libnghttp2 only. This is a short hand for --disable-app --disable-examples --disable-hpack-tools
+./configure --prefix=${PREFIX} --enable-python-bindings=no --enable-lib-only
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
+make check
