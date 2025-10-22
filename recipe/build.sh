c22eff13276d63bf513c6dff073e9d6305928643
Andrii Osipov <aosipov@anaconda.com>
2025-10-22 06:36:30 -0700

    try with gcc 14.3.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,8 @@
 cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
 
 # Build libnghttp2 only. This is a short hand for --disable-app --disable-examples --disable-hpack-tools
-./configure --prefix=${PREFIX} --enable-python-bindings=no --enable-lib-only
+./configure --prefix=${PREFIX} --enable-python-bindings=no
+#--enable-lib-only
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make check
