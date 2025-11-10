0ed77b4caa7da0513fdaae1c7f39244517a932a1
Andrii Osipov <aosipov@anaconda.com>
2025-11-10 06:26:41 -0800

    return bin dir

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,9 +2,7 @@
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
 
-# Build libnghttp2 only. This is a short hand for --disable-app --disable-examples --disable-hpack-tools
 ./configure --prefix=${PREFIX} --enable-python-bindings=no
-#--enable-lib-only
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make check
